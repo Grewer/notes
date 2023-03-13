21bef9cd0a82a502688c1741e5d907ab21bd5703
grewer <grewer@grewer.cn>
2023-03-14 02:01:43 +0800

    fix: 错误逻辑调整

--- create-directory.js
+++ create-directory.js
@@ -19,7 +19,7 @@ function readFileList(dir, filesList = []) {
         const fullPath = path.join(dir, item);
         const stat = getStatus(fullPath);
         if (stat.isDirectory) {
-            if(item.includes(ignoreDir) || item.startsWith('.')){
+            if(ignoreDir.includes(item) || item.startsWith('.')){
                 return
             }
             const obj = {name: item, children: []}
