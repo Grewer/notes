8b6fc071b6bb433f4c724fd8570acd37c0646f48
grewer <grewer@grewer.cn>
2023-03-29 23:55:57 +0800

    docs: quill.js 2

--- JS 知识点/富文本/quill.js 自定义扩展/example/src/App.tsx
+++ JS 知识点/富文本/quill.js 自定义扩展/example/src/App.tsx
@@ -2,14 +2,7 @@ import React, {useMemo, useRef, useState} from 'react';
 import './App.css';
 import ReactQuill, {Quill} from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import FindModal from "./FindModal";
-import SearchedStringBlot from './SearchedString'
 
-Quill.register(SearchedStringBlot);
-
-const CustomButton = () => <span className="iconfont">
-    find
-</span>;
 
 const CustomToolbar = () => <div id="toolbar">
     <select
@@ -23,46 +16,32 @@ const CustomToolbar = () => <div id="toolbar">
     </select>
     <button className="ql-bold"></button>
     <button className="ql-italic"></button>
-    <button className="ql-showFindModal">
-        <CustomButton/>
-    </button>
+    <button className="ql-link"></button>
 </div>
 
 
 function App() {
     const [value, setValue] = useState('');
-    const [visible, setVisible] = useState(false)
-    
-    function showFindModal() {
-        setVisible(true)
-    }
-    
+
     const modules = useMemo(() => ({
         toolbar: {
             container: '#toolbar',
             handlers: {
-                showFindModal,
+
             },
         },
     }), []);
-    
-    const _closeFindModal = () => setVisible(false)
+
     const editorRef = useRef<any>()
-    
+
     const getEditor = () => {
         return editorRef.current?.editor
     }
-    
+
     return (<div className={'container'}>
         <CustomToolbar/>
         {/*@ts-ignore*/}
         <ReactQuill ref={editorRef} theme="snow" value={value} modules={modules} onChange={setValue}/>
-        {visible ? (
-            <FindModal
-                closeFindModal={_closeFindModal}
-                getEditor={getEditor}
-            />
-        ) : null}
     </div>)
 }
 
