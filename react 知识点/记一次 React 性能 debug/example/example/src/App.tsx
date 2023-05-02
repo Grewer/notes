a4f951b83f8bcd52ae6b6ad14423e6c364fc2738
grewer <grewer@grewer.cn>
2023-05-03 00:12:21 +0800

    docs: debug2

---
+++ react 知识点/记一次 React 性能 debug/example/example/src/App.tsx
@@ -0,0 +1,53 @@
+import React, {useMemo, useRef, useState} from 'react';
+import './App.css';
+import ReactQuill, {Quill} from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const CustomButton = () => <span className="iconfont">
+    find
+</span>;
+
+const CustomToolbar = () => <div id="toolbar">
+    <select
+        className="ql-header"
+        defaultValue={''}
+        onChange={(e) => e.persist()}
+    >
+        <option value="1"></option>
+        <option value="2"></option>
+        <option selected></option>
+    </select>
+    <button className="ql-bold"></button>
+    <button className="ql-italic"></button>
+    <button className="ql-showFindModal">
+        <CustomButton/>
+    </button>
+</div>
+
+
+function App() {
+    const [value, setValue] = useState('');
+
+
+    const modules = useMemo(() => ({
+        toolbar: {
+            container: '#toolbar',
+            handlers: {
+            },
+        },
+    }), []);
+
+    const editorRef = useRef<any>()
+
+    const getEditor = () => {
+        return editorRef.current?.editor
+    }
+
+    return (<div className={'container'}>
+        <CustomToolbar/>
+        {/*@ts-ignore*/}
+        <ReactQuill ref={editorRef} theme="snow" value={value} modules={modules} onChange={setValue}/>
+    </div>)
+}
+
+export default App;
