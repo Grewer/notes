efdf3a54274929a1ce112b2569c17f2733acfc94
grewer <grewer@grewer.cn>
2023-05-07 01:06:14 +0800

    docs: debug4 一个 TODOlist, 模拟富文本外的其他场景

--- react 知识点/记一次 React 性能 debug/example/src/App.tsx
+++ react 知识点/记一次 React 性能 debug/example/src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useMemo, useRef, useState} from 'react';
-import 'src/App.css';
+import './App.css';
 import ReactQuill, {Quill} from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import useHooks from "./customHooks";
@@ -20,9 +20,6 @@ const CustomToolbar = () => <div id="toolbar">
     </select>
     <button className="ql-bold"></button>
     <button className="ql-italic"></button>
-    <button className="ql-showFindModal">
-        <CustomButton/>
-    </button>
 </div>
 
 
@@ -56,10 +53,84 @@ function App() {
         return editorRef.current?.editor
     }
 
+
+    const [todos, setTodos] = useState([
+        {
+            content: 'Pickup dry cleaning',
+            isCompleted: true,
+        },
+        {
+            content: 'Get haircut',
+            isCompleted: false,
+        },
+        {
+            content: 'Build a todo app in React',
+            isCompleted: false,
+        }
+    ]);
+
+    function handleKeyDown(e, i) {
+        if (e.key === 'Enter') {
+            createTodoAtIndex(e, i);
+        }
+        if (e.key === 'Backspace' && todos[i].content === '') {
+            e.preventDefault();
+            return removeTodoAtIndex(i);
+        }
+    }
+
+    function createTodoAtIndex(e, i) {
+        const newTodos = [...todos];
+        newTodos.splice(i + 1, 0, {
+            content: '',
+            isCompleted: false,
+        });
+        setTodos(newTodos);
+    }
+
+    function updateTodoAtIndex(e, i) {
+        const newTodos = [...todos];
+        newTodos[i].content = e.target.value;
+        setTodos(newTodos);
+    }
+
+    function removeTodoAtIndex(i) {
+        if (i === 0 && todos.length === 1) return;
+        setTodos(todos => todos.slice(0, i).concat(todos.slice(i + 1, todos.length)));
+    }
+
+    function toggleTodoCompleteAtIndex(index) {
+        const temporaryTodos = [...todos];
+        temporaryTodos[index].isCompleted = !temporaryTodos[index].isCompleted;
+        setTodos(temporaryTodos);
+    }
+
     return (<div className={'container'}>
         <CustomToolbar/>
         {/*@ts-ignore*/}
         <ReactQuill ref={editorRef} theme="snow" value={value} modules={modules} onChange={setValue}/>
+
+
+
+        <form className="todo-list">
+            <ul>
+                {todos.map((todo, i) => (
+                  <div className={`todo ${todo.isCompleted && 'todo-is-completed'}`}>
+                      <div className={'checkbox'} onClick={() => toggleTodoCompleteAtIndex(i)}>
+                          {todo.isCompleted && (
+                            <span>&#x2714;</span>
+                          )}
+                      </div>
+                      <input
+                        type="text"
+                        value={todo.content}
+                        onKeyDown={e => handleKeyDown(e, i)}
+                        onChange={e => updateTodoAtIndex(e, i)}
+                      />
+                  </div>
+                ))}
+            </ul>
+        </form>
     </div>)
 }
 
--- react 知识点/记一次 React 性能 debug/example/src/index.tsx
+++ react 知识点/记一次 React 性能 debug/example/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'src/index.css';
-import App from 'src/App';
-import reportWebVitals from 'src/reportWebVitals';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
