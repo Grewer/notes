f0b80b1aa2b020150bf09f8926305b36d8a3a7dd
grewer <grewer@grewer.cn>
2023-07-06 01:37:55 +0800

    docs: quill 插件 3

---
+++ JS 知识点/富文本/quill.js 插件/example/src/modules/index.ts
@@ -0,0 +1,87 @@
+export default class QuillResize {
+  private quill: any;
+
+  constructor(quill, options = {}) {
+    quill.resizer = this;
+    // save the quill reference and options
+    this.quill = quill;
+
+    // respond to clicks inside the editor
+    this.quill.root.addEventListener('mousedown', this.handleClick, false);
+
+    this.quill.on('text-change', this.handleChange);
+
+    this.quill.emitter.on('resize-edit', this.handleEdit);
+
+    this.quill.root.parentNode.style.position = this.quill.root.parentNode.style.position || 'relative';
+
+    this.selectedBlots = [];
+    this.quill.on('selection-change', this.addBlotsSelectedClass);
+
+    // setup modules
+    this.moduleClasses = this.options.modules;
+
+    this.modules = [];
+  }
+
+
+  handleClick(evt) {
+    let show = false;
+    let blot;
+    const target = evt.target;
+
+    if (target && target.tagName) {
+      blot = this.quill.constructor.find(target);
+      if (blot) {
+        show = this.judgeShow(blot, target);
+      }
+    }
+    if (show) {
+      evt.preventDefault();
+      // evt.stopPropagation()
+      return;
+    }
+    if (this.activeEle) {
+      // clicked on a non image
+      this.hide();
+    }
+  }
+
+  handleChange(delta, oldDelta, source) {
+    if (this.updateFromModule) {
+      this.updateFromModule = false;
+      return;
+    }
+
+    if (source !== 'user' || !this.overlay || !this.activeEle) return;
+    this.onUpdate();
+  }
+
+  handleEdit() {
+    if (!this.blot) return;
+    const index = this.blot.offset(this.quill.scroll);
+    this.hide();
+    this.quill.focus();
+    this.quill.setSelection(index, 1);
+  }
+
+  addBlotsSelectedClass(range) {
+    if (!range) {
+      this.removeBlotsSelectedClass();
+      this.selectedBlots = [];
+      return;
+    }
+
+    const leaves = this.quill.scroll.descendants(Parchment.LeafBlot, range.index, range.length);
+    const blots = leaves.filter((blot) => {
+      const canBeHandle = !!this.options.parchment[blot.statics.blotName];
+      if (canBeHandle) blot.domNode.classList.add(this.options.selectedClass);
+      return canBeHandle;
+    });
+    this.removeBlotsSelectedClass(blots);
+    this.selectedBlots = blots;
+  }
+
+
+
+}
