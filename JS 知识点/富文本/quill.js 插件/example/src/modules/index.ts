63d872616e1795974ca219a553ae122e5bc1823a
grewer <grewer@grewer.cn>
2023-07-14 01:40:12 +0800

    docs: quill 插件 7

--- JS 知识点/富文本/quill.js 插件/example/src/modules/index.ts
+++ JS 知识点/富文本/quill.js 插件/example/src/modules/index.ts
@@ -6,13 +6,13 @@ const Parchment = Quill.import('parchment');
 export default class QuillResize {
   private quill: any;
   private resizeModule: any;
+  private activeEle?: HTMLElement;
 
   constructor(quill, options = {}) {
     this.quill = quill;
 
     this.quill.root.addEventListener('mousedown', this.handleClick, false);
 
-    this.onCreate();
     // respond to clicks inside the editor
 
     // this.quill.on('text-change', this.handleChange);
@@ -30,8 +30,7 @@ export default class QuillResize {
     // this.modules = [];
   }
 
-  onCreate() {
-    console.log('onCreate')
+  initializeModules = () => {
     this.resizeModule = new Resize(this);
   }
 
@@ -57,6 +56,85 @@ export default class QuillResize {
       this.hide();
     }
   }
+
+  judgeShow(blot, target) {
+    let res = false;
+    if (!blot) return res;
+
+    if (!target && blot.domNode) target = blot.domNode;
+    const options = {
+      attribute: ['width', 'height'],
+      limit: {
+        minWidth: 10,
+        ratio: true, // 默认按照原图片比例 缩放
+      },
+    }
+    if (this.activeEle === target) return true;
+
+    const limit = options.limit || {};
+    if (!limit.minWidth || (limit.minWidth && target.offsetWidth >= limit.minWidth)) {
+      res = true;
+
+      if (this.activeEle) {
+        // we were just focused on another image
+        this.hide();
+      }
+      // keep track of this img element
+      this.activeEle = target;
+      this.blot = blot;
+      // clicked on an image inside the editor
+      this.show();
+    }
+
+    return res;
+  }
+
+  show() {
+    this.showOverlay();
+    this.initializeModules();
+    if (this.activeEle) this.activeEle.classList.add('active');
+  }
+
+  hide() {
+    this.hideOverlay();
+    this.removeModules();
+    if (this.activeEle && this.options.activeClass) this.activeEle.classList.remove(this.options.activeClass);
+    this.activeEle = undefined;
+    this.blot = undefined;
+  }
+
+  showOverlay() {
+    if (this.overlay) {
+      this.hideOverlay();
+    }
+
+    this.quill.setSelection(null);
+
+    // prevent spurious text selection
+    this.setUserSelect('none');
+
+    // Create and add the overlay
+    this.overlay = document.createElement('div');
+    // this.overlay.setAttribute('title', "Double-click to select image");
+    Object.assign(this.overlay.style, this.options.styles.overlay);
+    this.overlay.addEventListener('dblclick', this.handleEdit.bind(this), false);
+
+    this.quill.root.parentNode.appendChild(this.overlay);
+
+    this.hideProxy = () => {
+      if (!this.activeEle) return;
+      this.hide();
+    };
+    // listen for the image being deleted or moved
+    this.quill.root.addEventListener('input', this.hideProxy, true);
+
+    this.updateOverlayPositionProxy = this.updateOverlayPosition.bind(this);
+    this.quill.root.addEventListener('scroll', this.updateOverlayPositionProxy);
+
+    this.repositionElements();
+  }
+
+
 
   // handleChange(delta, oldDelta, source) {
   //   if (this.updateFromModule) {
